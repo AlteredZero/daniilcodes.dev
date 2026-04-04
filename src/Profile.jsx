d3f3f9b6c4745f16fc9b4f6e7a3ed361cd76e020
Altered <topzgameplayer@gmail.com>
2026-04-04 14:34:47 -0400

    add background gradiet animation and fixed some scaling issues. Plus small changes.

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,7 +2,7 @@ export default function Profile() {
   return(
     <div className="Profile"> 
         <h1 className="IntroductionText">Hi, I'm <br></br> Daniil Ovechkin</h1>
-        <img src="../assets/PersonalPicture.jpg" alt="A Picture Of Me" className="PersonalPicture"></img>
+        <img src="../assets/PersonalPicture2cropped.jpg" alt="A Picture Of Me" className="PersonalPicture"></img>
     </div>
   )
 }
