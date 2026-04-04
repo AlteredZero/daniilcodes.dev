74ccade9ddb53895d2034f2313fcecb9ee2fd6cb
Altered Games <topzgameplayer@gmail.com>
2026-04-04 18:30:55 -0400

    added introduction description

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,8 +1,11 @@
 export default function Profile() {
   return(
-    <div className="Profile"> 
-        <h1 className="IntroductionText">Hi, I'm <br></br> Daniil Ovechkin</h1>
-        <img src="../assets/PersonalPicture2cropped.jpg" alt="A Picture Of Me" className="PersonalPicture"></img>
+    <div className="Profile">
+      <div>
+        <h1 className="IntroductionHeaderText">Hi, I'm <br></br> Daniil Ovechkin</h1>
+        <p className="IntroductionText">I'm a full-stack developer and Unreal Engine 5 creator, building and shipping immersive games, web applications, and interactive systems, with a focus on performance, design, and cybersecurity fundamentals.</p>
+      </div>
+      <img src="../assets/PersonalPicture2cropped.jpg" alt="A Picture Of Me" className="PersonalPicture"></img>
     </div>
   )
 }
