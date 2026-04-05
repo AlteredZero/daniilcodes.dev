7ee4fbd21704088434ca58bbcc866160fa6e2e48
Altered <topzgameplayer@gmail.com>
2026-04-04 23:20:33 -0400

    added an entire 'about' section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Profile from "./Profile"
+import About from "./About"
 
 export default function App() {
   return(
     <div className="Main">
       <Header></Header>
       <Profile></Profile>
+      <About></About>
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return(
-    <div className="Header"> 
-        <button className="HeaderButtonHome">Daniil Ovechkin</button>
+    <div className="Header">
+        <img src="../assets/DaniilLogo.png" alt="Logo" className="HeaderLogo"></img>
         <button className="HeaderButton">About</button>
         <button className="HeaderButton">Projects</button>
         <button className="HeaderButton">Contact</button>
